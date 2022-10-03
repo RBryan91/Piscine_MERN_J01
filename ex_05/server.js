cdc9adbec55d111bd27d29d966f011c0bec98fc0
Renaudin Bryan <bryan.renaudin@epitech.eu>
2022-10-03 18:22:41 +0200

    Ex_05

--- ex_05/server.js
+++ ex_05/server.js
@@ -3,22 +3,19 @@ const app = express();
 const port = process.env.PORT || 4242;
 const fs = require('fs');
 const path = require('path');
-// 
-//  app.get('/name', function(req, res) {
-//    res.sendFile(path.join(__dirname, '/name.html'));
-//  });
 app . set ( 'view engine' , 'ejs' );
 
-app.get('/name/:name', (req, res) => {
-    var text =  req.params.name;
-    res.render('name', {text: text})
-  })
-  app.get('/name/', (req,res)=>
+app.get('/name/:name', (req, res) =>
+{
+    let name = req.params.name;
+    let age = req.query.age;
+
+    res.render("nameage", {name: name, age: age})
+});
+app.get('/name/', (req,res)=>
 {
     res.render('unknow');
 })
-   
-  
 app.listen(port, () => {
     console.log('Server app listening on port ' + port);
 });
